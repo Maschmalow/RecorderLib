dfdb5ea36462eaba03d00578583a8d40dbfb81cc
Maschmalow <maschmalow@gmail.com>
2019-07-05 00:31:22 +0200

    updated jda & remove obsolete bugfix

--- src/main/java/net/maschmalow/recorderlib/PCMBufferSender.java
+++ src/main/java/net/maschmalow/recorderlib/PCMBufferSender.java
@@ -2,23 +2,11 @@ package net.maschmalow.recorderlib;
 
 import net.dv8tion.jda.core.audio.AudioSendHandler;
 
-import java.util.Arrays;
 import java.util.Iterator;
-
-import static net.maschmalow.recorderlib.AudioLib.CHUNK_SIZE;
 
 
 public class PCMBufferSender implements AudioSendHandler {
 
-
-    //temporary workaround because discord is bugged (https://github.com/DV8FromTheWorld/JDA/issues/789)
-    //This flag should be set to false if the bug doesn't happen for whatever reason
-    //All the active code (including this flag) should be remove if the bug is fixed for good
-    private static final boolean AUDIO_BUGFIX_FLAG = true;
-    private static byte[] silence = new byte[CHUNK_SIZE];
-    static {
-        Arrays.fill(silence,(byte)0);
-    }
 
     private Iterator<byte[]> audioDataIter;
 
@@ -29,35 +17,19 @@ public class PCMBufferSender implements AudioSendHandler {
 
     @Override
     public boolean canProvide() {
-        if(AUDIO_BUGFIX_FLAG)
-            return true;
-        else {
-            if(audioDataIter == null) return false;
+        if(audioDataIter == null) return false;
 
-            boolean canProvide = audioDataIter.hasNext();
-            if(!canProvide)
-                audioDataIter = null; //clear all reference so that gc can do its job
+        boolean canProvide = audioDataIter.hasNext();
+        if(!canProvide)
+            audioDataIter = null; //clear all reference so that gc can do its job
 
-            return canProvide;
-        }
+        return canProvide;
+
     }
 
     @Override
     public byte[] provide20MsAudio() {
-
-        if(AUDIO_BUGFIX_FLAG) {
-            if(audioDataIter != null)
-                if(audioDataIter.hasNext())
-                    return audioDataIter.next();
-                else
-                    audioDataIter = null;
-
-
-            return silence;
-        } else {
-            return audioDataIter.next();
-        }
-
+        return audioDataIter.next();
     }
 
     @Override
